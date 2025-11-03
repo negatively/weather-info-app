1ecd17bb47c7048c0ec2394ec5c72c8150d8ce04
negatively <mileniandi39@gmail.com>
2025-11-03 15:02:49 +0700

    feat: create various modal

---
+++ src/renderer/src/components/AlertModal.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import BaseModal from './BaseModal.vue'
+
+const props = defineProps({
+    show: { type: Boolean, required: true },
+    title: { type: String, default: 'Alert' },
+    message: { type: [String, Object, Array], default: '' }
+})
+
+const emit = defineEmits(['update:show'])
+
+function close() {
+    emit('update:show', false)
+}
+</script>
+
+<template>
+    <BaseModal :show="props.show" :onClose="close">
+        <div class="max-w-md p-6">
+            <h3 class="text-lg font-semibold mb-2 text-center">{{ props.title }}</h3>
+            <div class="my-5 mx-2 text-sm">
+                <p v-if="typeof props.message === 'string'">{{ props.message }}</p>
+                <pre v-else class="whitespace-pre-wrap">{{ props.message }}</pre>
+            </div>
+            <div class="flex w">
+                <button @click="close" class="px-4 py-2 w-full bg-sky-600 hover:bg-sky-700 rounded">OK</button>
+            </div>
+        </div>
+    </BaseModal>
+</template>
---
+++ src/renderer/src/components/BaseModal.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Dialog, DialogPanel, TransitionRoot } from '@headlessui/vue'
+
+defineProps({
+    show: Boolean,
+    onClose: Function
+})
+</script>
+<template>
+    <TransitionRoot appear :show="show" as="template">
+        <Dialog as="div" class="relative z-50" @close="onClose">
+            <div class="fixed inset-0 bg-black/70 backdrop-blur-sm" aria-hidden="true" />
+            <div class="fixed inset-0 flex items-center justify-center p-4">
+                <DialogPanel class="bg-zinc-700 text-white rounded-lg overflow-hidden shadow-xl">
+                    <slot />
+                </DialogPanel>
+            </div>
+        </Dialog>
+    </TransitionRoot>
+</template>
